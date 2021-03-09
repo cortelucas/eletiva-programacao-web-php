c3fcb467a47d052bfa99155c32f30202900c7d34
Lucas Corte <lucas.corte@fatec.sp.gov.br>
2021-03-09 10:59:56 -0300

    - Implementação do exercício 5;

---
+++ lista-02/exe-05/index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Exercício 05</title>
+
+</head>
+<body>
+<header class="card-header bg-white text-center">
+    <h1>Exercício 05</h1>
+</header>
+<section class="container card shadow-lg mt-5 p-5">
+
+    <form method="post">
+        <div class="form-group row">
+            <label for="nota_01" class="col-sm-2 col-form-label">Digite a primeira nota: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="nota_01" name="nota_01">
+            </div>
+        </div>
+        <div class="form-group row mt-2">
+            <label for="nota_02" class="col-sm-2 col-form-label">Digite a segunda nota: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="nota_02" name="nota_02">
+            </div>
+        </div>
+        <div class="form-group row mt-2">
+            <label for="nota_03" class="col-sm-2 col-form-label">Digite a terceira nota: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="nota_03" name="nota_03">
+            </div>
+        </div>
+        <div class="form-group row mt-2">
+            <label for="nota_04" class="col-sm-2 col-form-label">Digite a quarta nota: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="nota_04" name="nota_04">
+            </div>
+        </div>
+
+
+        <input class="btn btn-outline-info mt-2" type="submit" value="Enviar">
+    </form>
+</section>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+-->
+</body>
+
+<div class="container mt-2 text-center">
+    <?php
+
+
+    if($_POST) {
+        $nota_01 = $_POST['nota_01'];
+        $nota_02 = $_POST['nota_02'];
+        $nota_03 = $_POST['nota_03'];
+        $nota_04 = $_POST['nota_04'];
+
+        $media = ($nota_01+$nota_02+$nota_03+$nota_04)/4;
+
+        if($media >= 7)
+            echo "Sua média foi $media. Parabéns, você foi Aprovado!";
+        else
+            echo "Sua média foi $media. Infelizmente, você foi Reprovado!";
+
+    }
+
+    ?>
+</div>
+</html>
