2a5c9bc7126fe11cb2e5288da29380b3c888e5ee
Lucas Corte <lucas.corte@fatec.sp.gov.br>
2021-03-09 10:52:14 -0300

    - Implementação do exercício 2;

---
+++ lista-02/exe-02/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
+
+    <title>Exercício 02</title>
+</head>
+<body>
+<header class="card-header bg-white text-center">
+    <h1>Exercício 02</h1>
+</header>
+<section class="container card shadow-lg mt-5 p-5">
+
+    <form method="post">
+        <div class="form-group row">
+            <label for="valor_kl" class="col-sm-2 col-form-label">Valor do Quilo: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="valor_kl" name="valor_kl">
+            </div>
+        </div>
+        <div class="form-group row mt-2">
+            <label for="qnt_kls" class="col-sm-2 col-form-label">Quantidade de Quilos: </label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="qnt_kls" name="qnt_kls">
+            </div>
+        </div>
+        <input class="btn btn-outline-info" type="submit" value="Enviar">
+    </form>
+</section>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+        crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+-->
+</body>
+
+<div class="container mt-2 text-center">
+    <?php
+
+
+    if ($_POST) {
+        $valor_kl = $_POST['valor_kl'];
+        $qnt_kls = $_POST['qnt_kls'];
+
+        echo "o valor a ser pago é: ", $qnt_kls * $valor_kl;
+    }
+
+    ?>
+</div>
+</html>
