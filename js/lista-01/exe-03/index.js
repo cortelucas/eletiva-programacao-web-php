8b053c433d99d3ec789ab1b123c5fb18b8736c42
cortelucas <corte1994.lc@gmail.com>
2021-04-06 22:15:36 -0300

    - implementação do exercicio 03

---
+++ js/lista-01/exe-03/index.js
@@ -0,0 +1,32 @@
+let $ = document.querySelector.bind(document);
+
+let btn = $('#btn');
+
+btn.addEventListener('click', (e)=> {
+    e.preventDefault();
+
+    let valor = Number($('#valor').value);
+    let campo = $('#campo');
+
+    if (valor > 10){
+        campo.innerHTML = `
+        <div class="alert alert-light" role="alert">
+            Número digitado foi ${valor}, e o mesmo é maior do que 10!
+        </div>
+        `
+    } else if (valor < 0) {
+        campo.innerHTML = `
+        <div class="alert alert-light" role="alert">
+            Número digitado foi $valor, e o mesmo é menor do que 10!
+        </div>
+        `
+    } else {
+        campo.innerHTML = `
+        <div class="alert alert-light" role="alert">
+            Número igual a 10!
+        </div>
+        `
+    }
+
+
+});
